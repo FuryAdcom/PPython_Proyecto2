ef2add82f8a66d925b1ae07acf40b7463094bfb1
FuryAdcom <fastidiosowindows@hotmail.com>
2021-01-20 19:28:30 -0400

    Inicio formulario

--- Proyecto2/sandwiches/views.py
+++ Proyecto2/sandwiches/views.py
@@ -2,6 +2,30 @@ from django.shortcuts import render
 
 from django.template import loader
 
-def index(request):
+from .models import *
 
-    return render(request, 'main/index.html')
+def index(request):
+    return render(request, 'main/getName.html')
+
+def formulario(request):
+    sd = Sandwich.objects.all()
+    ing = Ingrediente.objects.all()
+
+    if request.method == 'POST' and 'add' in request.POST: 
+        venta = request.POST['venta']
+        cliente = Cliente.objects.get(fk_venta=venta.id)
+
+        return render(request, 'main/sandwich.html', {'sd': sd, 'ing': ing})
+
+    elif request.method == 'POST' and 'confirm' in request.POST: 
+        venta = request.POST['venta']
+        cliente = Cliente.objects.get(fk_venta=venta.id)
+
+        return render(request, 'main/.html')
+
+    else:
+        venta = Venta.objects.create()
+
+        cliente = Cliente.objects.create(nombre=request.POST['name'], fk_venta=venta.id)
+
+        return render(request, 'main/sandwich.html', {'name': cliente.nombre, 'sd': sd, 'ing': ing, 'v':venta})

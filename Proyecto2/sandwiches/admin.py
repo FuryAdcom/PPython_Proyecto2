aab96c9dc64c820ac3fa0cb172bcf02c8cb8c6b9
gsolerh <32111787+gsolerh@users.noreply.github.com>
2021-02-02 09:34:13 -0400

    Panel de Admin (1)

--- Proyecto2/sandwiches/admin.py
+++ Proyecto2/sandwiches/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
 
+import datetime
+
 from .models import *
 
 class VentaAdmin(admin.ModelAdmin):
-    list_display = ('monto_total', 'fecha',)
+    ing = Ingrediente.objects.all()
+    ventas = Venta.objects.all()
+    sd = Sandwich.objects.all()
+
+    for venta in ventas:
+        cliente = Cliente.objects.get(fk_venta=venta)
+        cs = cliente.clisan_set.all()
+        csid = cs.values_list('id', flat=True)
+        ci = SanIng.objects.filter(fk_sandwich__in=list(csid))
+
+        for s in cs:
+            for x in sd:
+                if s.fk_sandwich.id == x.pk:
+                    for a in ci:
+                        if a.fk_sandwich.id == s.id:
+                            for y in ing:
+                                if a.fk_ingrediente.id == y.pk:
+                                    list_display = ('monto_total', 'fecha',)
+
 admin.site.register(Venta, VentaAdmin)
 
 admin.site.register(Cliente)

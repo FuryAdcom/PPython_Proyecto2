8e32e7bb5c8f52016b65747f67db6df83480fd6f
gsolerh <32111787+gsolerh@users.noreply.github.com>
2021-02-02 20:58:07 -0400

    Panel de Admin (2)

--- Proyecto2/sandwiches/admin.py
+++ Proyecto2/sandwiches/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.template.response import TemplateResponse
 from django.urls import path
-from django.contrib.admin import AdminSite
 
 import datetime
 
@@ -17,32 +16,36 @@ class VentaAdmin(admin.ModelAdmin):
         return my_urls + urls
 
     def my_view(self, request):
-        # ...
+        ing = Ingrediente.objects.all()
+        ventas = Venta.objects.all()
+        sd = Sandwich.objects.all()
+
+        lista = []
+
+        for venta in ventas:
+            cliente = Cliente.objects.get(fk_venta=venta)
+            cs = cliente.clisan_set.all()
+            csid = cs.values_list('id', flat=True)
+            ci = SanIng.objects.filter(fk_sandwich__in=list(csid))
+
+            count = 0
+            for s in cs:
+                count += 1
+                for x in sd:
+                    if s.fk_sandwich.id == x.pk:
+                        for a in ci:
+                            if a.fk_sandwich.id == s.id:
+                                for y in ing:
+                                    if a.fk_ingrediente.id == y.pk:
+                                        lista.append([venta.id, venta.monto_total, venta.fecha, cliente.nombre, count, x.size, y.nombre])
+
         context = dict(
            # Include common variables for rendering the admin template.
            self.admin_site.each_context(request),
-           # Anything else you want in the context...
+           lista = lista,
            key=1,
         )
         return TemplateResponse(request, "admin/ventas.html", context)
-    # ing = Ingrediente.objects.all()
-    # ventas = Venta.objects.all()
-    # sd = Sandwich.objects.all()
-
-    # for venta in ventas:
-    #     cliente = Cliente.objects.get(fk_venta=venta)
-    #     cs = cliente.clisan_set.all()
-    #     csid = cs.values_list('id', flat=True)
-    #     ci = SanIng.objects.filter(fk_sandwich__in=list(csid))
-
-    #     for s in cs:
-    #         for x in sd:
-    #             if s.fk_sandwich.id == x.pk:
-    #                 for a in ci:
-    #                     if a.fk_sandwich.id == s.id:
-    #                         for y in ing:
-    #                             if a.fk_ingrediente.id == y.pk:
-    #                                 
 
 admin.site.register(Venta, VentaAdmin)
 
